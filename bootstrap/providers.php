759a3fd452c2dc93b382c7051cdf70a321a3ea2a
sotiriskast <soteris100@gmail.com>
2025-01-17 12:50:06 +0200

    add debugin tools

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,9 +2,9 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
     App\Providers\MiddlewareProvider::class,
-    App\Providers\AuthServiceProvider::class,
-
+    App\Providers\TelescopeServiceProvider::class,
 ];
