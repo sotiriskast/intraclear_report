b319aac4ed2ad67ce21bf04bca72ac91ce435ab6
sotiriskast <soteris100@gmail.com>
2025-02-26 15:16:07 +0200

    loaded over insecure request

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
-
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
