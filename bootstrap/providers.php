6a0c56617a3701965e52270e4b20676c35c12f41
sotiriskast <soteris100@gmail.com>
2025-05-07 15:36:00 +0300

    dev - update providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,14 +1,19 @@
 <?php
 
-return [
+$providers = [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
     App\Providers\MiddlewareProvider::class,
     App\Providers\S3AssumeRoleServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
     App\Providers\ApiServiceProvider::class,
     Modules\Cesop\Providers\CesopServiceProvider::class,
 ];
+
+if (app()->environment('local')) {
+    $providers[] = App\Providers\TelescopeServiceProvider::class;
+}
+
+return $providers;
