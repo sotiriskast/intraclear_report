072eecea3483730197bc933057fb38efe7a4b3aa
sotiriskast <soteris100@gmail.com>
2025-06-23 12:37:03 +0300

    Modify visa issue download for listing the available files for download

--- Modules/MerchantPortal/resources/views/index.blade.php
+++ Modules/MerchantPortal/resources/views/index.blade.php
@@ -1,5 +1,5 @@
-<x-merchantportal::layouts.master>
-    <h1>Hello World</h1>
+{{--<x-merchantportal::layouts.master>--}}
+{{--    <h1>Hello World</h1>--}}
 
-    <p>Module: {!! config('merchantportal.name') !!}</p>
-</x-merchantportal::layouts.master>
+{{--    <p>Module: {!! config('merchantportal.name') !!}</p>--}}
+{{--</x-merchantportal::layouts.master>--}}
